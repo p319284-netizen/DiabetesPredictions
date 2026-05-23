731fd4f29dfa2028ba25a915f8a2c7177c010e51
MELISSA CIRENE OLIVAS PALMA <p319284@uach.mx>
2026-05-23 13:44:56 -0600

    Update input ranges for diabetes prediction inputs

--- DiabetesPredictions.py
+++ DiabetesPredictions.py
@@ -14,9 +14,6 @@ import sklearn
 from PIL import Image
 from sklearn.base import BaseEstimator, TransformerMixin
 
-col_names = "total_rooms", "total_bedrooms", "population", "households"
-rooms_ix, bedrooms_ix, population_ix, households_ix = [1,2,3,4]
-
 #def predictValue(some_data):
  #   with open('anaconda3/my_model1.pkl', 'rb') as f: 
   #      model = jb.load(f)
@@ -32,14 +29,14 @@ st.text('Dra. Olanda Prieto Ordaz')
 st.text('Melissa Cirene Olivas Palma - 319284')
 st.header('PREDICT DIABETES RISK PROBABILITY')
 
-attrPregnancies = st.selectbox('Select Pregnancies', np.arange(-130, -110, 2))
-attrGlucose = st.selectbox('Select Glucose', np.arange(30, 50, 2))
-attrBloodPressure = st.selectbox('Select BloodPressure', np.arange(1, 60, 1))
-attrSkinThickness = st.selectbox('Select SkinThickness', np.arange(2, 39320, 1))
-attrInsulin = st.selectbox('Select Insulin', np.arange(1, 6445, 1))
-attrBMI = st.selectbox('Select BMI', np.arange(1, 40000, 5))
-attrDiabetesPedigree = st.selectbox('Select DiabetesPedigreeFunction', np.arange(1, 6082, 1))
-attrAge = st.selectbox('Select Age', np.arange(0.0, 15.00, 0.1))
+attrPregnancies = st.selectbox('Select Pregnancies', np.arange(0, 20, 1))
+attrGlucose = st.selectbox('Select Glucose', np.arange(100, 250, 2))
+attrBloodPressure = st.selectbox('Select BloodPressure', np.arange(60, 260, 1))
+attrSkinThickness = st.selectbox('Select SkinThickness', np.arange(0, 100, 1))
+attrInsulin = st.selectbox('Select Insulin', np.arange(1, 850, 1))
+attrBMI = st.selectbox('Select BMI', np.arange(1, 70, 5))
+attrDiabetesPedigree = st.selectbox('Select DiabetesPedigreeFunction', np.arange(0.0, 3.0, 0.1))
+attrAge = st.selectbox('Select Age', np.arange(20, 85, 1))
 
 #if st.button('PREDICT VALUE'):
  #   data = pd.array([attrPregnancies, attrGlucose, attrBloodPressure, attrSkinThickness, attrInsulin, attrBMI, attrDiabetesPedigree, attrAge, 0])
